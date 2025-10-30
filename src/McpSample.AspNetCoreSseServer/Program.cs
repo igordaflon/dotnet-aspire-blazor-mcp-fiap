59a0ed58e9860f73803c10ec2adb03e8b0ed74aa
Igor Daflon <igordafloncouto@gmail.com>
2025-10-30 00:40:30 -0300

    Commit inicial

---
+++ src/McpSample.AspNetCoreSseServer/Program.cs
@@ -0,0 +1,27 @@
+using McpSample.AspNetCoreSseServer;
+using ModelContextProtocol;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add default services
+builder.AddServiceDefaults();
+builder.Services.AddProblemDetails();
+
+// add MCP server
+builder.Services
+    .AddMcpServer()
+    .WithHttpTransport()
+    .WithTools<Jokes>()
+    .WithTools<WeatherTool>();
+//.WithToolsFromAssembly();
+var app = builder.Build();
+
+// Initialize default endpoints
+app.MapDefaultEndpoints();
+app.UseHttpsRedirection();
+
+// map endpoints
+app.MapGet("/", () => $"Hello MCP Server! {DateTime.Now}");
+app.MapMcp();
+
+app.Run();
